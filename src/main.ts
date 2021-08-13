6124dd0b72b87dc8bb4279d16b3db3aad5a383c9
Johhny <jhasse20@hotmail.com>
2021-08-12 21:10:03 -0300

    correção em porta para deploy

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 
   const server = app.getHttpServer();
   const router = server._events.request._router;
