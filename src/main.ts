4e3f04830daf1c5e87bad6576d8d9cb6a179f874
Johhny <jhasse20@hotmail.com>
2021-08-20 02:28:39 -0300

    adicionado validações ao criar usuário

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+import { ValidationError, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationException } from './errors/validation.exception';
+import { ValidationFilter } from './errors/validation.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new ValidationFilter());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      skipMissingProperties: false,
+      exceptionFactory: (errors: ValidationError[]) => {
+        const messages = errors.map((error: ValidationError) => {
+          return {
+            message: Object.values(error.constraints)[0],
+          };
+        });
+        return new ValidationException(messages);
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT || 3000);
 
   const server = app.getHttpServer();
